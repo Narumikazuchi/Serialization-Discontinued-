ce5903061c963bbf605886e23b15154c2b0e29a9
Narumikazuchi <euwkamigoroshi@yahoo.de>
2022-01-01 20:52:19 +0100

    Added standard implemented types in documentation for default strategies.

--- Narumikazuchi.Serialization.Bytes/Building/Fluent Interfaces/IByteSerializerDeserializerDefaultStrategyAppender.cs
+++ Narumikazuchi.Serialization.Bytes/Building/Fluent Interfaces/IByteSerializerDeserializerDefaultStrategyAppender.cs
@@ -11,7 +11,11 @@ public interface IByteSerializerDeserializerDefaultStrategyAppender<TSerializabl
     /// </summary>
     /// <remarks>
     /// Default strategies will not override any prior custom strategies that have been configured. Furthermore every strategy targeting one
-    /// of the types that the default strategies target will overwrite the default strategy with the custom one.
+    /// of the types that the default strategies target will overwrite the default strategy with the custom one.<para/>
+    /// The default strategies include strategies for the following types: <see cref="Boolean"/>, <see cref="Byte"/>, <see cref="Char"/>,
+    /// <see cref="Double"/>, <see cref="Int16"/>, <see cref="Int32"/>, <see cref="Int64"/>, <see cref="IntPtr"/>, <see cref="SByte"/>,
+    /// <see cref="Single"/>, <see cref="UInt16"/>, <see cref="UInt32"/>, <see cref="UInt64"/>, <see cref="UIntPtr"/>, <see cref="DateTime"/>,
+    /// <see cref="DateOnly"/>, <see cref="TimeOnly"/>, <see cref="TimeSpan"/>, <see cref="Guid"/>, <see cref="Half"/>, <see cref="String"/>.
     /// </remarks>
     public IByteSerializerDeserializerStrategyAppenderOrFinalizer<TSerializable> UseDefaultStrategies();
 }
