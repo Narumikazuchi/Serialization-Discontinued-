c4dc449328c6f7b4b053a16df37f7fdb3055f594
Narumikazuchi <ganzel.davy@swschwedt.de>
2023-01-09 00:01:20 +0100

    Updated usings to now be inside the csproj file.

--- Narumikazuchi.Serialization/usings.cs
+++
@@ -1,9 +0,0 @@
-﻿global using Narumikazuchi.Collections;
-global using Narumikazuchi.InputOutput;
-global using System;
-global using System.Collections;
-global using System.Collections.Generic;
-global using System.Diagnostics.CodeAnalysis;
-global using System.Diagnostics.Contracts;
-global using System.IO;
-global using System.Linq;
